e41821e3c3a202edbfdccb693087f6e76e8a0b37
Aleksandr Starkov <77936849+AleksandrStark@users.noreply.github.com>
2022-05-31 23:31:46 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect,
+} from 'react-router-dom';
+import ServiceForm from './components/ServiceForm';
+import ServiceList from './components/ServiceList';
+
+function App() {
+	return (
+		<div className="App">
+			<div className="app-container">
+				<Router>
+					<Switch>
+						<Redirect exact from="/" to="/services" />
+						<Route exact path="/services" component={ServiceList} />
+						<Route exact path="/services/:id" component={ServiceForm} />
+					</Switch>
+				</Router>
+			</div>
+		</div>
+	);
+}
+
+export default App;
